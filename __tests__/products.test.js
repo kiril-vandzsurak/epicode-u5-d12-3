9ea3cde9bc778e072cc7ab93d6a060a25d560ff2
Kiril Vandzsurak <kiril.wandzurak@gmail.com>
2023-02-14 15:27:52 +0100

    minor changes

--- __tests__/products.test.js
+++ __tests__/products.test.js
@@ -3,18 +3,18 @@
 // On Windows you cannot use NODE_OPTIONS (and all env vars) from command line --> YOU HAVE TO USE CROSS-ENV PACKAGE TO BE ABLE TO PASS
 // ENV VARS TO COMMAND LINE SCRIPTS ON ALL OPERATIVE SYSTEMS!!!
 
-import supertest from "supertest"
-import dotenv from "dotenv"
-import mongoose from "mongoose"
-import server from "../src/server.js"
-import ProductsModel from "../src/api/products/model.js"
+import supertest from "supertest";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import server from "../src/server.js";
+import ProductsModel from "../src/api/products/model.js";
 
-dotenv.config() // This command forces .env vars to be loaded into process.env. This is the way to do it whenever you can't use -r dotenv/config
+dotenv.config(); // This command forces .env vars to be loaded into process.env. This is the way to do it whenever you can't use -r dotenv/config
 
 // supertest is capable of executing server.listen of our Express app if we pass the Express server to it
 // It will give us back a client that can be used to run http requests on that server
 
-const client = supertest(server)
+const client = supertest(server);
 
 /* describe("Test APIs", () => {
   it("Should test that GET /test endpoint returns 200 and a body containing a message", async () => {
@@ -29,42 +29,57 @@ const validProduct = {
   name: "A valid product",
   description: "balllablalblabl",
   price: 100,
-}
+};
 
 const notValidProduct = {
   name: "A not valid product",
   price: 100,
-}
+};
 
 beforeAll(async () => {
-  await mongoose.connect(process.env.MONGO_URL_TEST)
-  const product = new ProductsModel({ name: "test", description: "blalblabla", price: 20 })
-  await product.save()
-})
+  await mongoose.connect(process.env.MONGO_URL_TEST);
+  const product = new ProductsModel({
+    name: "test",
+    description: "blalblabla",
+    price: 20,
+  });
+  await product.save();
+});
 // beforeAll is a Jest hook ran before all the tests, usually it is used to connect to the db and to do some initial setup (like inserting some mock data in the db)
 
 afterAll(async () => {
-  await ProductsModel.deleteMany()
-  await mongoose.connection.close()
-})
+  await ProductsModel.deleteMany();
+  await mongoose.connection.close();
+});
 // afterAll hook could be used to clean up the situation (close the connection to Mongo gently and clean up db/collections)
 
 describe("Test APIs", () => {
   it("Should test that the env vars are set correctly", () => {
-    expect(process.env.MONGO_URL_TEST).toBeDefined()
-  })
+    expect(process.env.MONGO_URL_TEST).toBeDefined();
+  });
 
   it("Should test that POST /products returns a valid _id and 201", async () => {
-    const response = await client.post("/products").send(validProduct).expect(201)
-    expect(response.body._id).toBeDefined()
-  })
+    const response = await client
+      .post("/products")
+      .send(validProduct)
+      .expect(201);
+    expect(response.body._id).toBeDefined();
+  });
 
   it("Should test that GET /products returns a success status and a body", async () => {
-    const response = await client.get("/products").expect(200)
-    console.log(response.body)
-  })
+    const response = await client.get("/products").expect(200);
+    console.log(response.body);
+  });
 
   it("Should test that POST /products with a not valid product returns a 400", async () => {
-    await client.post("/products").send(notValidProduct).expect(400)
-  })
-})
+    await client.post("/products").send(notValidProduct).expect(400);
+  });
+
+  it("Should test that GET /products/:id returns the correct product with a valid id", async () => {
+    await client.get("/products/63eb95616435069f73f61610").expect(404);
+  });
+
+  // it("Should DELETE the /products/:id", async () => {
+  //   const delete = await client.delete
+  // })
+});
--- src/api/products/index.js
+++ src/api/products/index.js
@@ -1,25 +1,41 @@
-import express from "express"
-import ProductsModel from "./model.js"
+import express from "express";
+import ProductsModel from "./model.js";
+// import createHttpError from "../../errorHandlers.js";
 
-const productsRouter = express.Router()
+const productsRouter = express.Router();
 
 productsRouter.get("/", async (req, res, next) => {
   try {
-    const products = await ProductsModel.find()
-    res.send(products)
+    const products = await ProductsModel.find();
+    res.send(products);
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
 productsRouter.post("/", async (req, res, next) => {
   try {
-    const newProduct = new ProductsModel(req.body)
-    const { _id } = await newProduct.save()
-    res.status(201).send({ _id })
+    const newProduct = new ProductsModel(req.body);
+    const { _id } = await newProduct.save();
+    res.status(201).send({ _id });
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
-export default productsRouter
+productsRouter.get("/:id", async (req, res, next) => {
+  try {
+    const product = await ProductsModel.findById(req.params.id);
+    if (product) {
+      res.send(product);
+    } else {
+      next(
+        createHttpError(404, `Author with id ${req.params.authorId} not found!`)
+      );
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default productsRouter;
